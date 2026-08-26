eb46a9b3306ef79ea2be79bf01197cf2642c4aa2
magmoud <magmoud788@gmail.com>
2026-08-26 22:21:23 +0400

    fixed pool env files

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,6 @@
 const { Pool } = require("pg");
 require('dotenv').config();
 
-console.log("PGHOST:", process.env.PGHOST);
-console.log("PGDATABASE:", process.env.PGDATABASE);
-console.log("PGUSER:", process.env.PGUSER);
-console.log("PASSWORD TYPE:", typeof process.env.PGPASSWORD);
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
