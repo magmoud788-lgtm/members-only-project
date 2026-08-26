163dc5c9a2cda964ad9952ea73b3603634999aa0
magmoud <magmoud788@gmail.com>
2026-08-26 22:30:26 +0400

    check kdatabase environment

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,8 @@
 const { Pool } = require("pg");
-require('dotenv').config();
+
+require("dotenv").config();
+
+console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
