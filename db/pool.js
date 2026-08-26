8a81ce096e72c17ecbf717fd48b216b19dc8f1b3
magmoud <magmoud788@gmail.com>
2026-08-26 22:59:10 +0400

    added join and admin password

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,13 @@
 const { Pool } = require("pg");
+require('dotenv').config();
 
-require("dotenv").config();
-
-console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
+console.log("PGHOST:", process.env.PGHOST);
+console.log("PGDATABASE:", process.env.PGDATABASE);
+console.log("PGUSER:", process.env.PGUSER);
+console.log("PASSWORD TYPE:", typeof process.env.PGPASSWORD);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-});
+ });
 
 module.exports = pool;
--- db/populatedb.js
+++ db/populatedb.js
@@ -1,5 +1,4 @@
 const { Client } = require('pg');
-require('dotenv').config();
 
 const SQL = `
 CREATE TABLE IF NOT EXISTS users (
@@ -25,9 +24,7 @@ async function main() {
   console.log('seeding...')
   const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+
 });
 
   await client.connect();
