101066f8c063a3421887aebeeaccf3743c143c9b
magmoud <magmoud788@gmail.com>
2026-08-26 21:55:03 +0400

    fixed pool function

--- db/pool.js
+++ db/pool.js
@@ -7,11 +7,6 @@ console.log("PGUSER:", process.env.PGUSER);
 console.log("PASSWORD TYPE:", typeof process.env.PGPASSWORD);
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
   connectionString: process.env.DATABASE_URL,
 });
 
